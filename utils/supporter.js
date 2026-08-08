39c5b0150c8856370428a09814b1db316384da54
Kinan <kinan.chahid2011@gmail.com>
2026-08-08 07:48:02 +0400

    Fixed graphics for support v2.

--- utils/supporter.js
+++ utils/supporter.js
@@ -6,6 +6,7 @@ module.exports = ({ user, banner }) => {
       "<a:gvcsunspin:1527220557890850846> GVC Supporter <a:gvcsunspin:1527220557890850846>",
     description: `> <:arrowright:1534182706836144158> ${user} is showing support for **Greenville Community!** They have been given the <@&1472134913397493813> as a reward. You can only obtain this role by having /gvc in your status.`,
     banner,
+    noLogo: true, //
   });
 
   // Set thumbnail to the user's avatar
